76ffc2a7eb0f9d31bf5826839d0502f32e217296
Your Name <you@example.com>
2024-02-06 12:57:33 +0600

    change

--- src/components/TodoList/List.js
+++ src/components/TodoList/List.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { IoCheckmarkDoneSharp } from 'react-icons/io5'
 import { MdDone } from 'react-icons/md'
-
-
+import { MdDelete } from 'react-icons/md'
 
 const List = () => {
-  const [doneItems, setDoneItems] = useState(false)
+	const [doneItems, setDoneItems] = useState(false)
 	const { todo } = useSelector(todo => todo)
 	const { done } = useSelector(done => done)
 	const [bg, setBg] = useState(false)
@@ -18,13 +17,13 @@ const List = () => {
 		// console.log(del)
 	}
 
-function handleChange(id) {
-  	setDoneItems(el => ({ ...el, [id]: !el[id] }))
+	function handleChange(id) {
+		setDoneItems(el => ({ ...el, [id]: !el[id] }))
 
-    console.log(doneItems[id]);
-}
+		console.log(doneItems[id])
+	}
 
-// console.log(checkbox.target.value);
+	// console.log(checkbox.target.value);
 
 	// console.log(bg);
 
@@ -38,10 +37,28 @@ function handleChange(id) {
 						background: doneItems[el.id] ? 'MediumSeaGreen' : 'black'
 					}}
 					key={el.id}
-					className={`list w-96 flex items-center justify-between h-15  ease-out rounded-md p-3 
+					className={`list w-[1000px] flex items-start justify-start  h-15  ease-out rounded-md p-3 
 					 bg-gray-900 mb-5`}
 				>
-					<div className='todoText '>
+					<div className='flex items-center justify-center flex-col gap-2'>
+						<button
+							style={{
+								transition: '.3s'
+							}}
+							onClick={() => handleChange(el.id)}
+							class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-2.5 rounded-full'
+						>
+							{doneItems[el.id] ? <IoCheckmarkDoneSharp /> : <MdDone />}
+						</button>
+						<button
+							type='button'
+							onClick={() => DeleteTask(el.id)}
+							className='  focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 '
+						>
+							<MdDelete />
+						</button>
+					</div>
+					<div className='todoText px-10 pr-32 '>
 						<h1
 							style={{
 								color: 'white'
@@ -49,26 +66,6 @@ function handleChange(id) {
 						>
 							{el.title}
 						</h1>
-					</div>
-
-					<div className=''>
-						<button 
-            style={{
-              transition:'.3s'
-            }}
-							onClick={() => handleChange(el.id)}
-							class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1.5 px-2.5 rounded-full'
-						>
-							{doneItems[el.id] ? <IoCheckmarkDoneSharp /> : <MdDone />}
-						</button>
-
-						<button
-							type='button'
-							onClick={() => DeleteTask(el.id)}
-							className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 ml-2'
-						>
-							delete
-						</button>
 					</div>
 				</div>
 			))}
